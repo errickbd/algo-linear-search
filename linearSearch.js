d3f5a30838a57d6829dc3ec4e5ab21d378fd3ac5
Errick Basa-Denis <errickbd@gmail.com>
2024-01-31 16:38:47 -0600

    completed

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,21 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  if (arr.includes(searchTerm)){
+    return arr.indexOf(searchTerm)
+  }
+  
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let pushedArr = [];
+  for( let i = 0; i < arr.length; i++){
+    if (arr[i] === searchTerm){
+    pushedArr.push(i)
+  } 
+  }
+  return pushedArr
 }
+
+
+
 
 module.exports = { linearSearch, globalLinearSearch };
